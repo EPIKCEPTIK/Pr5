65e4d212fc3af89e2d5d9836af941db5d4164e67
EPIKCEPTIK <vitaliishm09@gmail.com>
2025-12-04 13:15:18 +0200

    Initial commit

---
+++ src/main/java/org/example/TestRecognizer.java
@@ -0,0 +1,30 @@
+package org.example;
+public class TestRecognizer {
+
+    public enum State {
+        S, ONE, TWO, THREE, F
+    }
+
+    public static State process(String input) {
+        var currentState = State.S;
+
+        if (input == null) return State.S;
+
+        for (char c : input.toCharArray()) {
+            currentState = switch (currentState) {
+                case S -> (c == 'T') ? State.ONE : State.S;
+
+                case ONE -> (c == 'E') ? State.TWO
+                        : (c == 'T') ? State.ONE : State.S;
+
+                case TWO -> (c == 'S') ? State.THREE
+                        : (c == 'T') ? State.ONE : State.S;
+
+                case THREE -> (c == 'T') ? State.F : State.S;
+
+                case F -> State.F;
+            };
+        }
+        return currentState;
+    }
+}
---
+++ src/test/java/TestRecognizerTest.java
@@ -0,0 +1,25 @@
+import org.example.TestRecognizer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TestRecognizerTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "TEST,          F",
+            "abc,           S",
+            "TTEST,         F",
+            "TTTEST,        F",
+            "TETEST,        F",
+            "TTETEST,       F",
+            "TESTEST,       F",
+            "TESA_TEST,     F",
+            "TE_TEST,       F",
+    })
+    void testLogic(String input, TestRecognizer.State expected) {
+        assertEquals(expected, TestRecognizer.process(input));
+    }
+}
